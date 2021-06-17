7b07377a78f7c1557880f3ac4378e8fd6fbea2f0
natane010 <mimikaumi@hotmail.com>
2021-06-18 05:26:29 +0900

    1

---
+++ Get_Start_Unity/Assets/Scripts/Terrain/TerrainNaviMeshBake.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TerrainNaviMeshBake : MonoBehaviour
+{
+    GameObject gameObject;
+    Player player;
+    //--------------------------------------------
+    //navimesh
+    NavMeshSurface navMeshSurface;
+    NavMeshAgent navMeshAgent;
+    //--------------------------------------------
+    public void SetNavMeshComponent()
+    {
+        gameObject = GameObject.Find("Terrain");
+        navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
+        navMeshSurface = gameObject.AddComponent<NavMeshSurface>();
+    }
+    public void SetNavMesh()
+    {
+        navMeshSurface.BuildNavMesh();
+    }
+}
